afc86b6ddea7f36701411ddbc11ec3543bf4cb97
Chirayu Desai <chirayudesai1@gmail.com>
2019-01-14 19:37:56 +0530

    jasmine_sprout: Add initial Calyx makefile

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,13 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_jasmine_sprout.mk
+    $(LOCAL_DIR)/lineage_jasmine_sprout.mk \
+    $(LOCAL_DIR)/calyx_jasmine_sprout.mk
 
 COMMON_LUNCH_CHOICES := \
     lineage_jasmine_sprout-user \
     lineage_jasmine_sprout-userdebug \
-    lineage_jasmine_sprout-eng
+    lineage_jasmine_sprout-eng \
+    calyx_jasmine_sprout-user \
+    calyx_jasmine_sprout-userdebug \
+    calyx_jasmine_sprout-eng
