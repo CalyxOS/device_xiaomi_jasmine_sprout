4f23e6c28e95b8ed8ade31fd7f8c2a42377e145c
Michael Bestas <mkbestas@gmail.com>
2021-07-14 17:46:11 +0300

    Exclude generic APN/NFC configs
    Change-Id: Ie7e29e947622cd0058da463d64440db777a9fb01

--- calyx_jasmine_sprout.mk
+++ calyx_jasmine_sprout.mk
@@ -14,6 +14,9 @@
 # limitations under the License.
 #
 
+# Exclude generic APN/NFC configs
+TARGET_EXCLUDE_GENERIC_CONFIGS := true
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
