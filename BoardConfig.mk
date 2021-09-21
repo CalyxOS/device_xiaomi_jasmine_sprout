7882f8a4ceddc471d7fd4fc3246cafe663a02219
Michael Bestas <mkbestas@gmail.com>
2021-09-21 16:28:23 +0300

    jasmine_sprout: Set firmware and kernel security patch levels
    * vendor comes from last available stock update, which is July 2021.
    * firmware comes from September 2020 update, since Xiaomi made it
      impossible to upgrade to newer firmware without requiring a data wipe.
    * kernel gets updated with latest monthly QCOM tags and android-4.4-p
      updates, so it matches platform security patch level.
    
    Change-Id: Ibbccc44ec64773ff61e28eb303a78d28073d6d85

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,6 +24,9 @@ TARGET_NO_RECOVERY := true
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
+# Properties
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 
